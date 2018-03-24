77f57663fdca583a92428cbc980884ac5d982ccd
Atila Fassina <atila@fassina.eu>
2018-03-24 14:32:34 +0100

    add load more button

--- components/PostList.js
+++ components/PostList.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Link from 'next/link'
 import {colors} from '../config'
 
@@ -7,93 +8,127 @@ function getDate (string) {
   return `${dateObj.getFullYear()}-${dateObj.getMonth()}-${dateObj.getDay()}`
 }
 
-export default ({posts}) => (
-  <div className="wrapper">
-    <ul className="list">
-      {posts && posts.map( ({title, link, categories, date, image}) => (
-        <li className="post" key={title}>
-          <Link href={link}>
-            <a target="_blank">
-              <h2 className="title">{title}</h2>
-              <div>
-                <img className="image" src={image} />
-              </div>
-              <span className="date">{getDate(date)}</span>
-            </a>
-          </Link>
-        </li>
-      ))}
-    </ul>
-    <style jsx>{`
-      .wrapper {
-        background-color: rgb(245,245,245);
-      }
+export default class PostList extends React.Component {
 
-      .list {
-        width: 90%;
-        list-style: none;
-        margin: 0 auto;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-around;
-      }
+  state = {
+    showPosts: 6
+  }
 
-      .post {
-        width: 40ex;
-        min-height: 45ex;
-        margin: 5ex 0.5ex;
-        background-color: white;
-        display: flex;
-        flex-flow: column;
-        justify-content: space-around;
-        align-items: center;
-        border: 3px solid ${colors.BERLINER_SKY};
-        animation: fadeUp 1.5s;
-      }
+  showMorePosts = () => {
+    this.setState(() => ({ showPosts: this.state.showPosts + 6 }))
+  }
+  
+  render ()  {
+    const {posts=[]} = this.props
 
-      .post:hover,
-      .post:active {
-        border-top-color: ${colors.RADIOACTIVE_LIME};
-        border-bottom-color: ${colors.RADIOACTIVE_LIME};
-      }
+    return (
+      <div className="wrapper">
+        <ul className="list">
+          {posts.slice(0,this.state.showPosts).map( ({title, link, categories, date, image}) => (
+            <li className="post" key={title}>
+              <Link href={link}>
+                <a target="_blank">
+                  <h2 className="title">{title}</h2>
+                  <div>
+                    <img className="image" src={image} />
+                  </div>
+                  <span className="date">{getDate(date)}</span>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <button className='loadButton' onClick={this.showMorePosts}>load more</button>
+        <style jsx>{`
+          .wrapper {
+            background-color: rgb(245,245,245);
+            display: flex;
+            flex-direction: column;
+          }
 
-      .post a {
-        display: block;
-        height: 100%;
-        width: 100%;
-        text-decoration: none;
-      }
+          .list {
+            width: 90%;
+            list-style: none;
+            margin: 0 auto;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+          }
 
-      .date {
-        padding: 2ex 0;
-        display: block;
-        text-align: center;
-        color: ${colors.BRUSHED_STEEL};
-      }
+          .post {
+            width: 40ex;
+            min-height: 45ex;
+            margin: 5ex 0.5ex;
+            background-color: white;
+            display: flex;
+            flex-flow: column;
+            justify-content: space-around;
+            align-items: center;
+            border: 3px solid ${colors.BERLINER_SKY};
+            animation: fadeUp 1.5s;
+          }
 
-      .title {
-        color: ${colors.BRUSHED_STEEL};
-        margin-bottom: 2ex;
-        height: ${1.5 * 3}em;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-      }
+          .post:hover,
+          .post:active {
+            border-top-color: ${colors.RADIOACTIVE_LIME};
+            border-bottom-color: ${colors.RADIOACTIVE_LIME};
+          }
 
-      .image { width: 100%; }
+          .post a {
+            display: block;
+            height: 100%;
+            width: 100%;
+            text-decoration: none;
+          }
 
-      @keyframes fadeUp {
-        0% {
-          opacity: 0;
-          transform: translateY(35px)
-        }
+          .date {
+            padding: 2ex 0;
+            display: block;
+            text-align: center;
+            color: ${colors.BRUSHED_STEEL};
+          }
 
-        100% {
-          opacity: 1;
-          transform: translateY(0)
-        }
-      }
-    `}</style>
-  </div>
-)
+          .title {
+            color: ${colors.BRUSHED_STEEL};
+            margin-bottom: 2ex;
+            height: ${1.5 * 3}em;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            text-align: center;
+          }
+
+          .image { width: 100%; }
+
+          .loadButton {
+            margin: 3ch auto 5ch;
+            padding: 2ch 4ch;
+            font-size: 1.2em;
+            background-color: ${colors.BERLINER_SKY};
+            border: 3px solid white;
+            cursor: pointer;
+          }
+
+          .loadButton:hover,
+          .loadButton:active {
+            background-color: white;
+            border-top-color: ${colors.RADIOACTIVE_LIME};
+            border-bottom-color: ${colors.RADIOACTIVE_LIME};
+          }
+
+          @keyframes fadeUp {
+            0% {
+              opacity: 0;
+              transform: translateY(35px)
+            }
+
+            100% {
+              opacity: 1;
+              transform: translateY(0)
+            }
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
