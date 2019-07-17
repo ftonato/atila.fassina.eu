0b6a2565a4683908b4d51c7543796467d56a84c6
Atila Fassina <atila@fassina.eu>
2019-07-18 00:08:31 +1200

    add aria-label to logo link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,10 +3,10 @@ import { Link } from 'gatsby'
 import Logo from '../Logo'
 import { Top, LogoWrap } from './styles'
 
-export default ({ data, internal = false }) => (
+export default ({ internal = false }) => (
   <Top internal={internal}>
     <LogoWrap internal={internal}>
-      <Link to="/">
+      <Link to="/" aria-label="Go to frontpage: Atila Fassina - Web Engineer">
         <Logo color={internal ? '#fff' : '#000'} />
       </Link>
     </LogoWrap>
