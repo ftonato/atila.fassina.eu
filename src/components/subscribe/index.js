04712e9498049ee53008965d7ffafe7d0731f4c3
Atila Fassina <atila@fassina.eu>
2019-07-19 21:13:55 +1200

    create component with mailchimp subscribe form elements

---
+++ src/components/subscribe/index.js
@@ -0,0 +1,17 @@
+export default () => (
+  <form
+    action="https://fassina.us3.list-manage.com/subscribe/post"
+    method="POST"
+  >
+    <input type="hidden" name="u" value="ffa1a5e8ef4db5b2506125bbf" />
+    <input type="hidden" name="id" value="9e9a87a7b0" />
+    <input
+      type="email"
+      name="b_email"
+      tabindex="-1"
+      value=""
+      placeholder="youremail@gmail.com"
+      id="b_email"
+    />
+  </form>
+)
